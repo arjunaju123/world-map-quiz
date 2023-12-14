1be159104251751b6a0b91112d5e86c3b045a3c9
Ashish Sam T George <ashish.george@experionglobal.local>
2023-12-14 14:17:42 +0530

    fix: Remove Conflict

--- game.js
+++ game.js
@@ -8,16 +8,10 @@ const answers=[
 "Europe", 
 "Africa", 
 "North America", 
-<<<<<<< HEAD
 "South America"
 ];
 
 // let currentQuestion = 0;         
-=======
-"South America"];
-
-let currentQuestion = 0;         
->>>>>>> 2a5845699ab11aab6d358cdbd73043cdf65dc93a
 let score = 0;
 
 const checkAnswer = (clickedContinent) =>  {
@@ -25,17 +19,10 @@ const checkAnswer = (clickedContinent) =>  {
 
     if (clickedContinent == correctAnswer) {    
         score++;
-<<<<<<< HEAD
         document.getElementById('result').innerHTML = 'Correct Answer';
     }//to print statement in the div with id result
         else {
         
             document.getElementById('result').innerHTML = 'Wrong Answer';
-=======
-        document.getElementById('result').innerHTML = 'Correct Answer';}//to print statement in the div with id result
-        else {
-        
-            document.getElementById('result').innerHTML = 'Wrong Answer';;
->>>>>>> 2a5845699ab11aab6d358cdbd73043cdf65dc93a
         }
     }
